4d38ad55667ca931bf98f25a0a383d9ba388222a
SahanHasintha <sahanhasintha12345@gmail.com>
2025-12-17 16:50:22 +1000

    changes in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -10,7 +10,7 @@ export class UserService {
   async register(createUserDto: CreateUserDto) {
     const { name, email, password } = createUserDto;
 
-    // Check if user already exist
+    // Check if user already exists
     const existingUser = await this.prisma.user.findUnique({
       where: { email },
     });
