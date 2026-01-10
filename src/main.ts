d3e55716a676c00114e72e86097fbae7fe95a7fe
SahanHasintha <sahanhasintha12345@gmail.com>
2026-01-10 23:43:50 +1000

    Update .gitignore to include Elastic Beanstalk files, modify package.json to change start script and add postinstall script, and change server port in main.ts from 3001 to 8080.

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   // Global prefix
   app.setGlobalPrefix('api');
 
-  const port = process.env.PORT || 3001;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
   
   console.log(`🚀 Server running on http://localhost:${port}`);
